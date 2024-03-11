d27233242a3470dbc25e091c17d29ae7bfd660a9
deepak022001 <studydipu@gmail.com>
2024-03-11 23:51:25 +0530

    Successfully Build Header Component

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import Header from '@/components/Header';
+export default function Home() {
+  return (
+    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
+      <Head>
+        <title>Home-Netflix</title>
+        <link
+          rel='stylesheet'
+          href='/favicon.ico'
+        />
+      </Head>
+      <Header />
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+        {/* modal */}
+      </main>
+    </div>
+  );
+}
